ff3391ed58aad5441b0d968836e8617cdf181149
m2web <m2web@yahoo.com>
2018-04-11 03:48:41 -0400

    Added integration tests for the EsvService

---
+++ IntegrationTests/EsvServiceIntegrationTests.cs
@@ -0,0 +1,29 @@
+using System;
+using NUnit.Framework;
+using myApp.Services;
+
+namespace UnitTests
+{
+    //run at the command prompt with the following command:
+    //dotnet test --filter TestCategory=Integration
+    [TestFixture, Description("Integration Tests")]
+    [Category("Integration")]
+    public class EsvServiceIntegrationTests {
+        
+        [Test]
+        public void GetDailyVerseAsync_Should_not_Be_Empty(){
+            System.Diagnostics.Debug.WriteLine("Starting GetDailyVerseAsync_Should_not_Be_Empty");
+            var esvService = new EsvService();
+            var output = esvService.GetDailyVerseAsync().Result;
+            Assert.IsNotEmpty(output);
+        }
+
+         [Test]
+        public void GetTodaysPsalmAsync_Should_not_Be_Empty(){
+            System.Diagnostics.Debug.WriteLine("Starting GetDailyVerseAsync_Should_not_Be_Empty");
+            var esvService = new EsvService();
+            var output = esvService.GetTodaysPsalmAsync().Result;
+            Assert.IsNotEmpty(output);
+        }
+    }
+}
