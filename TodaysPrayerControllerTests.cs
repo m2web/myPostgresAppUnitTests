1f4480c336b6d07ba1b0a656428d75585fc1629e
m2web <m2web@yahoo.com>
2018-04-15 21:01:09 -0400

    Added new controller test. Note that not completed yet.

--- TodaysPrayerControllerTests.cs
+++ TodaysPrayerControllerTests.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using myApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Identity;
 
 namespace UnitTests
 {
@@ -16,10 +17,12 @@ namespace UnitTests
     //[Ignore("Ignoring these tests")]
     [TestFixture, Description("Unit Tests")]
     [Category("Unit")]
-    public class HomeControllerTests{
+    public class TodayPrayerControllerTests{
 
         Westminstercatechism catechism = new Westminstercatechism();
         List<Jmverse> jmVerseList = new List<Jmverse>();
+        List<Prayerrequest> prayerRequests = new List<Prayerrequest>();
+        AppSettings appSettings = new AppSettings();
         
         [SetUp]
         public void Setup() { 
@@ -50,12 +53,33 @@ namespace UnitTests
                     Enddate = It.IsAny<DateTime>()
                 }
             };
+
+            prayerRequests = new List<Prayerrequest>{
+                new Prayerrequest(){
+                    Id = It.IsAny<int>(),
+                    Category = It.IsAny<string>(),
+                    Request = It.IsAny<string>(),
+                    Userid = It.IsAny<string>()
+                },
+                 new Prayerrequest(){
+                    Id = It.IsAny<int>(),
+                    Category = It.IsAny<string>(),
+                    Request = It.IsAny<string>(),
+                    Userid = It.IsAny<string>()
+                }
+            };
+
+            appSettings = new AppSettings(){
+                ValleyOfVisionURLBase = It.IsAny<string>()
+            };
         }
 
         [TearDown]
         public void TearDown() {
             catechism = null;
             jmVerseList = null;
+            prayerRequests = null;
+            appSettings = null;
         }
 
         [Test]
@@ -65,19 +89,37 @@ namespace UnitTests
             var catechismServiceMock = new Mock<IWestminsterCatechismService>();
             catechismServiceMock.Setup(x => x.GetTodaysCatechism()).Returns(catechism);
             //setup JMVerse Service xctor input parameter
-            var JmversesMock = new Mock<IJmVersesService>();
-            JmversesMock.Setup(x => x.GetYearsVerses(true)).Returns(jmVerseList);
+            var jmversesMock = new Mock<IJmVersesService>();
+            jmversesMock.Setup(x => x.GetYearsVerses(true)).Returns(jmVerseList);
 
-            var sut = new HomeController(catechismServiceMock.Object, JmversesMock.Object);
+            var prayerRequestServiceMock = new Mock<IPrayerRequestService>();
+            prayerRequestServiceMock.Setup(x => x.GetTodaysPrayerRequests(It.IsAny<string>())).Returns(prayerRequests);
+
+            var appSettingsMock = new Mock<IOptions<AppSettings>>();
+            appSettingsMock.Setup(x => x.Value).Returns(appSettings);
+
+            var userManagerMock = GetMockUserManager();
+
+            var sut = new TodaysPrayerController(jmversesMock.Object, catechismServiceMock.Object,
+                prayerRequestServiceMock.Object, appSettingsMock.Object, userManagerMock.Object);
             
             var indexResult = sut.Index();
-            var prayerResult = sut.Prayer();
+            var vovPrayerLinkResult = sut.GetVovPrayerLink();
+            var selectedPrayerResult = sut.GetSelectedPrayer(It.IsAny<int>());
             
-            Assert.IsNotNull(prayerResult);
+            Assert.IsNotNull(vovPrayerLinkResult);
             Assert.IsNotNull(indexResult);
+            Assert.IsNotNull(selectedPrayerResult);
             Assert.IsInstanceOf<ViewResult>(indexResult);
-            Assert.IsInstanceOf<ViewResult>(prayerResult);
-            
+            Assert.IsInstanceOf<ViewResult>(vovPrayerLinkResult);
+            Assert.IsInstanceOf<ViewResult>(selectedPrayerResult);
+        }
+
+        private Mock<UserManager<ApplicationUser>> GetMockUserManager()
+        {
+            var userStoreMock = new Mock<IUserStore<ApplicationUser>>();
+            return new Mock<UserManager<ApplicationUser>>(
+                userStoreMock.Object, null, null, null, null, null, null, null, null);
         }
     }
 }
